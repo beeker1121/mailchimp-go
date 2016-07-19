a7dccb8a97a7daba74ec9f0ac9851c7c849b9e64
Conner Hewitt <connerh87@gmail.com>
2016-07-19 13:12:10 -0500

    Added time.Time object to UpdateParams struct

--- lists/members/members.go
+++ lists/members/members.go
@@ -187,9 +187,9 @@ type UpdateParams struct {
 	VIP             bool                   `json:"vip,omitempty"`
 	Location        *Location              `json:"location,omitempty"`
 	IPSignup        string                 `json:"ip_signup,omitempty"`
-	TimestampSignup string                 `json:"timestamp_signup,omitempty"`
+	TimestampSignup time.Time              `json:"timestamp_signup,omitempty"`
 	IPOpt           string                 `json:"ip_opt,omitempty"`
-	TimestampOpt    string                 `json:"timestamp_opt,omitempty"`
+	TimestampOpt    time.Time              `json:"timestamp_opt,omitempty"`
 	EmailAddress    string                 `json:"email_address,omitempty"`
 	StatusIfNew     string                 `json:"status_if_new,omitempty"`
 }
