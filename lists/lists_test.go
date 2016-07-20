ca8f3d8d29497ca05a5f3996b7342916a739f0f1
Conner Hewitt <connerh87@gmail.com>
2016-07-19 20:47:03 -0500

    Added basic lists and members tests

--- lists/lists_test.go
+++ lists/lists_test.go
@@ -1,1 +1,31 @@
 package lists
+
+import (
+	"bytes"
+	"encoding/json"
+	"reflect"
+	"testing"
+	"time"
+)
+
+var timeString = "2020-01-02 23:59:59 +0000 UTC"
+var timeType = reflect.TypeOf(time.Time{})
+
+func TestListUnmarshal(t *testing.T) {
+	data := []byte(`{
+		"date_created": "2020-01-02 23:59:59"
+	}`)
+
+	list := &List{}
+	if err := json.NewDecoder(bytes.NewBuffer(data)).Decode(list); err != nil {
+		t.Error(err)
+	}
+
+	if reflect.TypeOf(list.DateCreated) != timeType {
+		t.Errorf("Expected list.DateCreated to be of type time.Time, got %v", reflect.TypeOf(list.DateCreated))
+	}
+
+	if list.DateCreated.String() != timeString {
+		t.Errorf("Expected list.DateCreated.String() to equal %s, got %s", timeString, list.DateCreated.String())
+	}
+}
--- lists/members/members_test.go
+++ lists/members/members_test.go
@@ -1,1 +1,31 @@
 package members
+
+import (
+	"bytes"
+	"encoding/json"
+	"reflect"
+	"testing"
+	"time"
+)
+
+var timeString = "2020-01-02 23:59:59 +0000 UTC"
+var timeType = reflect.TypeOf(time.Time{})
+
+func TestMemberUnmarshal(t *testing.T) {
+	data := []byte(`{
+		"timestamp_signup": "2020-01-02 23:59:59"
+	}`)
+
+	member := &Member{}
+	if err := json.NewDecoder(bytes.NewBuffer(data)).Decode(member); err != nil {
+		t.Error(err)
+	}
+
+	if reflect.TypeOf(member.TimestampSignup) != timeType {
+		t.Errorf("Expected member.TimestampSignup to be of type time.Time, got %v", reflect.TypeOf(member.TimestampSignup))
+	}
+
+	if member.TimestampSignup.String() != timeString {
+		t.Errorf("Expected member.TimestampSignup.String() to equal %s, got %s", timeString, member.TimestampSignup.String())
+	}
+}
