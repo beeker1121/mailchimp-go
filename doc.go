479f7624725466639abbe49ea59a8e92c44e0fc8
Conner Hewitt <connerh87@gmail.com>
2016-07-20 20:14:04 -0500

    Added main godoc file

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Package mailchimp is a Go client for the MailChimp API v3.
+//
+// Each API resource is a separate package within mailchimp-go.
+//
+// Below are the GoDoc references for each supported resource:
+//
+// Lists - https://godoc.org/github.com/beeker1121/mailchimp-go/lists
+// Lists/Members - https://godoc.org/github.com/beeker1121/mailchimp-go/lists/members
+package mailchimp
